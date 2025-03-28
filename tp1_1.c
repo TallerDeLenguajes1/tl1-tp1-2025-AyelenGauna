046acca73e272b5ea8a7af034f041873cfd37d1a
Dana Ayelen Gauna <ayelengauna564@gmail.com>
2025-03-28 20:25:22 -0300

    Ejercicio 3e

--- tp1_1.c
+++ tp1_1.c
@@ -1,6 +1,17 @@
 #include <stdio.h> 
 
 int main (){
-    printf("Hola Mundo");
+  
+  printf("Hola Mundo\n");
+
+  int numero = 4, *pnumero;
+  pnumero = &numero;
+  
+  printf("El contenido de lo que apunta el puntero: %d\n", *pnumero);
+  printf("Lo que contiene de dato el puntero: %p\n", pnumero);
+  printf("La direccion de memoria de la vble numero: %p\n", &numero);
+  printf("Lo que contiene de dato la vble numero: %d\n", numero);
+  printf("Tamaño de la vble numero: %lu bytes\n", sizeof(numero));
+
   return 0;  
 }
