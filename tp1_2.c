2aa7593b0008683311ddd5b47e2a20c67a9909ab
Dana Ayelen Gauna <ayelengauna564@gmail.com>
2025-03-28 23:16:50 -0300

    Ejercicio 4a

--- tp1_2.c
+++ tp1_2.c
@@ -1,18 +1,31 @@
 #include <stdio.h>
+#include <math.h>
 
-int cuadrado(int n);
+int cuadrado(int n); //a) Haga una función que devuelva el cuadrado de un número
+void cuadradoVoid(int n); //b) Haga la función anterior, pero devolviendo un tipo void
 
 int main() {
-    int num1,num2, resultado;
+    int num1,num2;
 
     printf("\nIngrese un numero para saber su cuadrado: ");
     scanf("%d",&num1);
 
     printf("\nSu cuadrado es: %d", cuadrado(num1));
 
+    printf("\nIngrese otro numero para saber su cuadrado: ");
+    scanf("%d",&num2);
+
+    cuadradoVoid(num2);
+
     return 0;
 }
 
 int cuadrado(int n){
-    return n * n;
+    return pow(n,2);
+}
+
+void cuadradoVoid(int n){ //c) Al recibir una variable muestre por pantalla la dirección y el contenido de la variable
+    printf("\nSu cuadrado es: %d", pow(n,2));
+    printf("\nEl contenido de la variable: %d", n);
+    printf("\nSu direccion de memoria: %p", &n);
 }
